c6cf53286ffba728f30c8ff11d721a633ad9ba91
anto <irianto223@gmail.com>
2017-11-09 02:09:52 +0700

    DB pakai mongoDB atlas 'telkom-testing'

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var bodyParser = require('body-parser');
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://localhost/telkom`)
+// mongoose.connect(`mongodb://localhost/telkom`)
+
+mongoose.connect(`mongodb://gananggww:Fn2yRagWtCPhdWIJ@cluster0-shard-00-00-rcykk.mongodb.net:27017,cluster0-shard-00-01-rcykk.mongodb.net:27017,cluster0-shard-00-02-rcykk.mongodb.net:27017/telkom-testing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`, {useMongoClient: true})
 
 var index = require('./routes/index');
 var users = require('./routes/users');
