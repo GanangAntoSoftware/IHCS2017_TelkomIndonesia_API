45e6ebd97c2f2169033c27fc343dd495f71adbfd
anto <irianto223@gmail.com>
2017-11-09 01:33:37 +0700

    fix typo

--- routes/votes.js
+++ routes/votes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const votesController = require('../controllers/votes')
+const votesController = require('../controllers/voteControllers')
 
 router.post('/', votesController.create)
 
